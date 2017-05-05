b2e8fe267f43b4aea77b7e08d00460974e505c95
i-am-batou <olivierlaine.social@hotmail.com>
2017-05-05 02:21:48 +0200

    project-20170505

---
+++ public/stylesheets/bootstrap/js/on-click-menu-button.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.menu-button').click(function(e){
+        e.preventDefault();
+        $this = ($this);
+        if($this.hasClass('menu-opened')){
+            $this.addClass('menu-closed').removeClass('menu-opened');
+        }else{
+            $this.removeClass('menu-closed').addClass('menu-opened');
+        }
+    });
+});
