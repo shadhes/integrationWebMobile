0d175c8927b6804b32a015db4ff949e00dd61b37
Barreau <barreau.gui@gmail.com>
2017-05-07 15:43:58 +0200

    07052017grunt

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: [{
+          "expand": true,
+          "cwd": "public/stylesheets/scss/",
+          "src": ["*.scss"],
+          "dest": "public/stylesheets/css",
+          "ext": ".css"
+        }]
+      }
+    },
+    watch: {
+      styles: {
+        files: 'public/stylesheets/scss/*.scss', // All FILES ext.scss
+        tasks: ['sass:dist']
+      }
+  },
+  concat_css: {
+    options: {
+      // specific options
+    },
+    all: {
+        src: ["public/stylesheets/css/*.css"],
+        dest: "public/stylesheets/css/compiled.css"
+    },
+  }
+
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-concat-css');
+  grunt.registerTask('default', ['sass:dist'])
+}
---
+++ public/stylesheets/js/on-click-menu-button.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $('.menu-button').click(function(e){
+        e.preventDefault();
+        $this = $(this);
+        if($this.hasClass('menu-opened')){
+            $this.addClass('menu-closed').removeClass('menu-opened');
+        }else{
+            $this.removeClass('menu-closed').addClass('menu-opened');
+        }
+    });
+
+    var url = "stylesheets/css/";
+    let fetchStyle = function(url) {
+      return new Promise((resolve, reject) => {
+        let link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.onload = function() { resolve(); console.log('style has loaded'); };
+        link.href = url;
+
+        let headScript = document.querySelector('script');
+        headScript.parentNode.insertBefore(link, headScript);
+      });
+    };
+
+});
+
+console.log('coucou je suis bien chargé !')
